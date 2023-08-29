47288ff6e49b3ba76ec7a16a60639374078b3548
AqiBoot01 <aqiboot007@gmail.com>
2023-08-29 14:56:42 +0500

    Raw node and modules

---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+
+    // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+
+
+//   sending html file back to client
+//   fs.readFile('./views/index.html', (err, data)=>{
+//     if(err) {
+//         console.log(err)
+//         res.end()
+//     }
+//     else {
+//         res.write(data)
+//         res.end();
+//     }
+// } )
+
+
+//   routing to multiple paths
+
+  let path = './views';
+
+  switch(req.url){
+    case '/':
+        path += '/index.html'
+        break;
+
+    case '/about' :
+        path += '/about.html'
+        break;
+
+    default :
+         path += '/404.html'
+         break;
+  }
+
+    //  reading the file based on the path 
+    fs.readFile(path, (err, data)=>{
+        if(err)
+         {
+            console.log(err)
+            res.end()
+         }
+        else 
+         {
+            res.write(data)
+            res.end(data);
+
+         }
+    })
+
+});
+
+
+
+
+server.listen(3000,'localhost', ()=>{
+  
+    console.log('server is listining');
+
+} )
+
+
